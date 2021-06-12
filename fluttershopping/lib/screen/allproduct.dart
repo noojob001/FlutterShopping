56ae14e458b013d8b8ea070e6428149b31c46ab7
noojob001 <noojob001@outlook.com>
2021-06-12 13:18:34 +0700

    lsyout all prod

--- fluttershopping/lib/screen/allproduct.dart
+++ fluttershopping/lib/screen/allproduct.dart
@@ -4,6 +4,7 @@ import 'package:fluttershopping/model/cartmodel.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 
+// ignore: must_be_immutable
 class AllProduct extends StatelessWidget {
   static String routeName = "/AllProduct";
 List<Product> _products = [
@@ -211,9 +212,9 @@ List<Product> _products = [
    @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo[50],
+      backgroundColor: Colors.amber[100],
       appBar: AppBar(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.orange[300],
         title: Text("All Product"),
         actions: <Widget>[
           IconButton(
@@ -224,24 +225,26 @@ List<Product> _products = [
       ),
       body:
       GridView.builder(
-        padding: EdgeInsets.all(8.0),
-        itemCount: _products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 2, mainAxisSpacing: 8, crossAxisSpacing: 8, childAspectRatio: 0.8),
-        itemBuilder: (context, index){
-          return ScopedModelDescendant<CartModel>(
-              builder: (context, child, model) {
-            return Card( child: Column( children: <Widget>[
-              Image.asset(_products[index].images[0], height: 120, width: 120,),
-              Text(_products[index].title, style: TextStyle(fontWeight: FontWeight.bold),),
-              Text("\$"+_products[index].price.toString()),
-              // ignore: deprecated_member_use
-              OutlineButton(
-                    child: Text("Add to Cart"),
-                    onPressed: () => model.addProduct(_products[index]))
-            ])); 
-          });
-        },
-      ),
+          padding: EdgeInsets.all(8.0),
+          itemCount: _products.length,
+          scrollDirection: Axis.horizontal,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 14, crossAxisSpacing: 8, childAspectRatio: 0.97),
+          itemBuilder: (context, index){
+            return ScopedModelDescendant<CartModel>(
+        builder: (context, child, model) {
+      return Card( child: Column( children: <Widget>[
+        Image.asset(_products[index].images[0], height: 120, width: 120,),
+        Text(_products[index].title, style: TextStyle(fontWeight: FontWeight.bold),),
+        Text("\$"+_products[index].price.toString()),
+        // ignore: deprecated_member_use
+        OutlineButton(
+              child: Text("Add to Cart"),
+              onPressed: () => model.addProduct(_products[index]))
+      ])); 
+            });
+          },
+        ),
+      
 
       // ListView.builder(
       //   itemExtent: 80,
