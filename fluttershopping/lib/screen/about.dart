7b7e0d38304cf1b994e36fa4bf0b470e06185b10
noojob001 <noojob001@outlook.com>
2021-06-16 10:07:33 +0700

    circle about

--- fluttershopping/lib/screen/about.dart
+++ fluttershopping/lib/screen/about.dart
@@ -16,11 +16,24 @@ class _AboutmeState extends State<Aboutme> {
       body: Column(
         children: <Widget>[
           Stack(
+            // ignore: deprecated_member_use
+            overflow: Overflow.visible,
             alignment: Alignment.center,
             children: <Widget>[
-              Image(image:NetworkImage('https://cw.lnwfile.com/_/cw/_raw/01/o1/yq.jpg'),height: 150,fit:BoxFit.fill)
+              Image(image:NetworkImage('https://cw.lnwfile.com/_/cw/_raw/01/o1/yq.jpg'),height: MediaQuery.of(context).size.height/4,fit:BoxFit.fill),
+              Positioned(
+                bottom:-35.0,
+                child: CircleAvatar(radius: 60, backgroundColor:  Colors.white,backgroundImage: NetworkImage('https://cw.lnwfile.com/_/cw/_resize/200/200/gv/qd/i3.jpg'),
+                )
+                ),
+              
             ],
-          )
+          ),
+          SizedBox(height:40),
+              ListTile(
+                title: Text("โรงงานบะหมี่เฮียลิ้ม (บางรัก)",style:TextStyle(fontSize: 18)),
+                subtitle:Text("79 ถนนจรัสเวียง แขวงสีลม เขตบางรัก กรุงเทพมหานคร 10500",style:TextStyle(fontSize: 18)),
+              )
         ],
       )
       
