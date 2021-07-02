23c4a3a598962b48d43076b6c64725c612312bd6
noojob001 <noojob001@outlook.com>
2021-07-02 19:22:19 +0700

    a

--- fluttershopping/lib/main.dart
+++ fluttershopping/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershopping/BeforeLogin/beforelogin.dart';
 // ignore: unused_import
@@ -13,7 +14,9 @@ import 'package:fluttershopping/shop.dart';
 import 'package:provider/provider.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp(
     model:CartModel(),
   ));
